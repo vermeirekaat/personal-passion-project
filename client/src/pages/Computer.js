1db61356c64a6a1b2f9093e040d44430a526a412
vermeirekaat <kaat.vermeire@telenet.be>
2021-11-18 12:26:56 +0100

    create seperate pages for Computer and Player #8

---
+++ client/src/pages/Computer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Computer = () => {
+    return (
+        <h2>Computer Screen</h2>
+    )
+};
+
+export default Computer;
---
+++ client/src/pages/Player.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Player = () => {
+
+    return (
+        <h2>Player Screen</h2>
+    )
+};
+
+export default Player;
