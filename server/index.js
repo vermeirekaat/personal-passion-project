dd4ae05f282dd8579839e25fdd818b1b98ba79bc
vermeirekaat <kaat.vermeire@telenet.be>
2021-11-18 10:57:51 +0100

    setup server with socket.io.io #4

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+    cors: {
+        origin: "http://localhost:3000",
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`new connection ${socket.id}`);
+
+    socket.on("disconnect", () => {
+        console.log(`someone has left`);
+    })
+});
+
+io.listen(3000);
