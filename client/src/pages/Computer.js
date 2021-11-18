3698fbe09d42ccb297e1c6cf011090bb6d9cbae0
vermeirekaat <kaat.vermeire@telenet.be>
2021-11-18 14:47:01 +0100

    initiate computer with code to check if identical #10

--- client/src/pages/Computer.js
+++ client/src/pages/Computer.js
@@ -7,7 +7,7 @@ const Computer = ({ socket }) => {
     const code = Math.floor(1000 + Math.random() * 9000);
 
     useEffect(() => {
-        socket?.emit("newUser", username, code);
+        socket?.emit("initialComputer", username, code.toString());
     }, [socket, username, code]);
 
     return (
@@ -15,7 +15,6 @@ const Computer = ({ socket }) => {
             <h2>Computer Screen</h2>
             <p>{code}</p>
         </div>
-       
     )
 };
 
--- client/src/pages/Player.js
+++ client/src/pages/Player.js
@@ -4,20 +4,15 @@ import { useState, useEffect } from "react";
 
 const Player = ({ socket }) => {
     const [username, setUsername] = useState("");
-    // const [code, setCode] = useState("");
 
     useEffect(() => {
       socket?.emit("newUser", username);
     }, [socket, username]);
 
-    // useEffect(() => {
-    //     socket?.emit("insertCode", code);
-    // }, [socket, username, code]);
-
-
     const generateCode = code => {
         socket?.emit("insertCode", username, code);
-    }
+    };
+    
     return (
         <div>
             <h2>Player Screen</h2>
