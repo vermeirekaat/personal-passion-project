c9e1fab57dd159a43c0d996f028523fe1ce59f6b
vermeirekaat <kaat.vermeire@telenet.be>
2021-11-23 10:11:19 +0100

    use express server #14

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
-import { Server } from "socket.io";
-// const fs = require('fs');
-import fs from 'fs';
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const port = process.env.PORT || 5000;
 
-const io = new Server({
+const io = require('socket.io')(server, {
     cors: {
-        // origin: "http://192.168.0.252:3000",
-        origin: "http://localhost:3000",
+        // origin: "https://192.168.0.252:3000",
+        origin: "http://localhost:3000", 
+        methods: ["GET", "POST"],
     }, 
     options: {
         key: fs.readFileSync('./localhost.key'),
         cert: fs.readFileSync('./localhost.crt')
-    }
+     }
 });
 
 let onlineUsers = [];
@@ -79,7 +82,12 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         removeUser(socket.id);
-    })
+    });
+
+    // app.use(express.static('public')); 
+    // server.listen(port, () => {
+    //     console.log(`App listening on port ${port}`);
+    // })
 });
 
-io.listen(5000);
+io.listen(port);
