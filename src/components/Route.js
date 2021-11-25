10313cc636681b0c9378f0ede778f447c82708cd
vermeirekaat <kaat.vermeire@telenet.be>
2021-11-25 11:54:55 +0100

    create component Route #12

---
+++ src/components/Route.js
@@ -0,0 +1,12 @@
+import styles from "./Route.module.css";
+
+const Route = () => {
+
+    return (
+        <div className={styles.container}>
+            <p>Route</p>
+        </div>
+    )
+}; 
+
+export default Route;
--- src/pages/Captain.js
+++ src/pages/Captain.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Avatar from "../components/Avatar";
+import Lives from "../components/Lives";
+import Route from "../components/Route";
+import Morse from "../components/Morse";
+import Result from "../components/Result";
+import Obstacle from "../components/Obstacle";
+import CheatSheet from "../components/CheatSheet";
 
 const Captain = ({ socket }) => {
     
     return (
         <div>
             <h2>Captain Screen</h2>
+            <Avatar/>
+            <Lives/>
+            <Route/>
+            <Morse/>
+            <Result/>
+            <Obstacle/>
+            <CheatSheet/>
         </div>
     )
 };
