71ccf1e247577282614df2ecaeccc42bee1ae84a
vermeirekaat <kaat.vermeire@telenet.be>
2021-11-25 11:51:20 +0100

    create component Obstacle #12

---
+++ src/components/Obstacle.js
@@ -0,0 +1,12 @@
+import styles from "./Obstacle.module.css";
+
+const Obstacle = () => {
+    
+    return (
+        <div className={styles.container}>
+            <p>Obstacle</p>
+        </div>
+    )
+}; 
+
+export default Obstacle;
