a8a8e627f6b68f7eb5e41567790b6e8afb3f4b46
vermeirekaat <kaat.vermeire@telenet.be>
2021-11-25 11:29:17 +0100

    add style to Lives #12

--- src/components/Lives.js
+++ src/components/Lives.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./Lives.module.css";
 
 const Lives = () => {
 
     return (
-        <div>
+        <div className={styles.container}>
             <p>3 lives</p>
         </div>
     )
