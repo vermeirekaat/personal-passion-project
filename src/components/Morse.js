80b5643b77b241a36e02428c525cc43007e4fdb2
vermeirekaat <kaat.vermeire@telenet.be>
2021-11-26 11:50:36 +0100

    display input of morse #11

--- src/components/Morse.js
+++ src/components/Morse.js
@@ -1,9 +1,29 @@
 import styles from "./Morse.module.css";
+import { useState } from "react";
 
 const Morse = () => {
+    
+    const [input, setInput] = useState([]);
+
+    const handleMorseCode = (e) => {
+        const copy = [...input];
+
+            if (e.key === "a") {
+                copy.push(".");
+                setInput(copy);
+            };
+            if (e.key === "z") {
+                copy.push("-");
+                setInput(copy);
+            }; 
+    };
+
+    console.log(input);
     return (
         <div className={styles.container}>
             <p>Morse Code</p>
+            <p>{input.toString()}</p>
+            <input readOnly onKeyPress={handleMorseCode}></input>
         </div>
     )
 }; 
