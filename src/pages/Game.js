c08ef235491d9c1d92e90b0f1eb86e8e18f26054
vermeirekaat <kaat.vermeire@telenet.be>
2021-12-03 16:41:30 +0100

    add page Game to project #30

---
+++ src/pages/Game.js
@@ -0,0 +1,119 @@
+import { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../context/Store";
+import styles from "./Game.module.css";
+
+import Lives from "../components/Lives";
+import Route from "../components/Route";
+import Storm from "../components/Storm";
+import Morse from "../components/Morse";
+import Result from "../components/Result";
+import Obstacle from "../components/Obstacle";
+import Options from "../components/Options";
+import CheatSheet from "../components/CheatSheet";
+
+const Game = ({ socket }) => {
+
+    // eslint-disable-next-line
+    const [state, dispatch] = useContext(Context);
+
+    let currentUser;
+    // let socketId;
+    if (state.users.length > 0) {
+        currentUser = state.users[0].user;
+        // socketId = state.users[0].socket;
+    }
+
+    const [route, setRoute] = useState("");
+    const [input, setInput] = useState("");
+    const [result, setResult] = useState("");
+
+    const handleInput = (input) => {
+        socket?.emit("morseInput", input);
+    };
+
+    const handleDirection = (direction) => {
+        socket?.emit("direction", direction);
+    }
+
+    useEffect(() => {
+        socket?.on("inputMorse", (data) => {
+            setInput(data);
+        });
+    }, [socket]);
+
+    useEffect(() => {
+        socket?.on("direction", (direction) => {
+            setRoute(direction);
+        });
+    }, [socket]);
+
+    useEffect(() => {
+        socket?.on("stepsMessage", (message) => {
+            console.log(message);
+        });
+    }, [socket]);
+
+    useEffect(() => {
+        socket?.on("result", (message) => {
+            setResult(message);
+        });
+    }, [socket]);
+
+    if (currentUser === "captain") {
+        return (
+            <div className={styles.grid}>
+                <div className={styles.lives}>
+                    <Lives/>
+                </div>
+                <div className={styles.route}>
+                    <Route currentDirection={route}/>
+                </div>
+                <div className={styles.morse}>
+                    <Morse morseCode={(input) => handleInput(input)}/>
+                </div>
+                <div className={styles.result}>
+                    <Result result={result}/>
+                </div>
+                <div className={styles.obstacle}>
+                    <Obstacle/>
+                </div>
+                <div className={styles.cheatsheet}>
+                    <CheatSheet/>
+                </div>
+            </div>
+        )
+    }; 
+
+    if (currentUser === "sailor") {
+        return (
+            <div className={styles.grid}>
+                <div className={styles.lives}>
+                    <Lives/>
+                </div>
+                <div className={styles.storm}>
+                    <Storm/>
+                </div>
+                <div className={styles.morse }>
+                    <Morse morseInput={input}/>
+                </div>
+                <div className={styles.result}>
+                    <Result result={result}/>
+                </div>
+                <div className={styles.options}>
+                    <Options/>
+                </div>
+                <div className={styles.cheatsheet}>
+                    <CheatSheet/>
+                </div>
+            </div>
+        )
+    };
+
+
+    return (
+        <Link to="/">No user was found</Link>
+    )
+}; 
+
+export default Game;
