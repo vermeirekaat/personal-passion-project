fc38a3373c4d49d59e8277d22955be55225317aa
vermeirekaat <kaat.vermeire@telenet.be>
2021-12-04 12:33:29 +0100

    fill cheatsheet with morse #53

--- src/components/CheatSheet.js
+++ src/components/CheatSheet.js
@@ -1,10 +1,119 @@
 import styles from "./CheatSheet.module.css"; 
 
 const CheatSheet = () => {
+
+    const morse = [
+        {
+            "letter": "A",
+            "morse": ".-",
+        },
+        {
+            "letter": "B",
+            "morse": "-...",
+        },
+        {
+            "letter": "C",
+            "morse": "-.-.",
+        },
+        {
+            "letter": "D",
+            "morse": "-..",
+        },
+        {
+            "letter": "E",
+            "morse": ".",
+        },
+        {
+            "letter": "F",
+            "morse": "..-.",
+        },
+        {
+            "letter": "G",
+            "morse": "--.",
+        },
+        {
+            "letter": "H",
+            "morse": "....",
+        },
+        {
+            "letter": "I",
+            "morse": "..",
+        },
+        {
+            "letter": "J",
+            "morse": ".---",
+        },
+        {
+            "letter": "K",
+            "morse": "-.-",
+        },
+        {
+            "letter": "L",
+            "morse": ".-..",
+        },
+        {
+            "letter": "M",
+            "morse": "--",
+        },
+        {
+            "letter": "N",
+            "morse": "-.",
+        },
+        {
+            "letter": "O",
+            "morse": "---",
+        },
+        {
+            "letter": "P",
+            "morse": ".--.",
+        },
+        {
+            "letter": "Q",
+            "morse": "--.-",
+        },
+        {
+            "letter": "R",
+            "morse": ".-.",
+        },
+        {
+            "letter": "S",
+            "morse": "...",
+        },
+        {
+            "letter": "T",
+            "morse": "-",
+        },
+        {
+            "letter": "U",
+            "morse": "..-",
+        },
+        {
+            "letter": "V",
+            "morse": "...-",
+        },
+        {
+            "letter": "W",
+            "morse": ".--",
+        },
+        {
+            "letter": "X",
+            "morse": "-..-",
+        },
+        {
+            "letter": "Y",
+            "morse": "-.--",
+        },
+        {
+            "letter": "Z",
+            "morse": "--..",
+        },
+    ]
     
     return (
         <div className={styles.container}>
-            <p>CheatSheet</p>
+            {morse.map((item) => (
+                <p className={styles.letter }key={item.letter}>{item.letter} <span>{item.morse}</span></p>
+            ))}
         </div>
     )
 }; 
