10b6cf6abf14026908dd200480bbe93862a3b1a2
vermeirekaat <kaat.vermeire@telenet.be>
2021-12-04 12:58:36 +0100

    remove comment lines

--- server/index.js
+++ server/index.js
@@ -86,7 +86,6 @@ const generateRoute = () => {
     for (let i = 0; i < 4; i++) {
         route.push(getRandomIndex(directions));
     };
-    // console.log(route);
     setTimeout(() => {
         emitRoute();
     }, 5000);
@@ -113,24 +112,6 @@ const generateObstacles = () => {
     const sailor = getUserByUsername("sailor");
     io.to(sailor.socketId).emit("message", "wait for message");
 }
-
-/* function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-} */
 
 const emitRoute = () => {
     const captain = getUserByUsername("captain");
