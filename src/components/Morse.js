e69f4317ad873f4f4cf161b4fd100548071aada4
vermeirekaat <kaat.vermeire@telenet.be>
2021-12-13 21:38:03 +0100

    style morse code input #18 78

--- src/components/Morse.js
+++ src/components/Morse.js
@@ -1,11 +1,23 @@
 import styles from "./Morse.module.css";
 
+import { usersContext } from "../context/Users";
+import { useContext } from "react";
+
 const Morse = ({ morseInput }) => {
 
+    // eslint-disable-next-line no-unused-vars
+    const [users, setUsers] = useContext(usersContext);
+    const colors = users.colors;
+
     return (
-        <div className={styles.container}>
-            <p>Morse Code</p> 
-            <p>{morseInput}</p>
+        <div className={styles.container} style={{ borderColor: colors.dark}}>
+            <div className={styles.inside} style={{ borderColor: colors.reg}}>
+
+                {morseInput !== "" ? 
+                <p className={styles.text} style={{ color: colors.reg}}>{morseInput}</p> : 
+                <p className={styles.text} style={{ color: colors.reg}}>Morse Code</p>}
+
+            </div>
         </div>
     )
 }; 
