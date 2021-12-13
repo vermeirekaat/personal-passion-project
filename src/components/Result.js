f9fc942674f3c79d4f90bbea4c972763726b6b87
vermeirekaat <kaat.vermeire@telenet.be>
2021-12-13 21:42:02 +0100

    add style to result component #18 #78

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,24 @@
 import styles from "./Result.module.css";
 
+import { usersContext } from "../context/Users";
+import { useContext } from "react";
+
 const Result = ({ result }) => {
-    return (
-        <div className={styles.container}>
-            <p>Resultaat</p>
-            <p>{result}</p>
-        </div>
-    )
+     // eslint-disable-next-line no-unused-vars
+     const [users, setUsers] = useContext(usersContext);
+     const colors = users.colors;
+ 
+     return (
+         <div className={styles.container} style={{ borderColor: colors.dark}}>
+             <div className={styles.inside} style={{ borderColor: colors.reg}}>
+ 
+                 {result !== "" ? 
+                 <p className={styles.text} style={{ color: colors.reg}}>{result}</p> : 
+                 <p className={styles.text} style={{ color: colors.reg}}>Result</p>}
+ 
+             </div>
+         </div>
+     )
 }; 
 
 export default Result;
