edb1fea4ad13de7842837534086191c716c2a870
vermeirekaat <kaat.vermeire@telenet.be>
2021-12-13 21:00:59 +0100

    add design to lives component #18 #78

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,11 +1,29 @@
 import styles from "./Route.module.css";
+import { usersContext } from "../context/Users";
+import { useContext } from "react";
+
+import dirRight from "./../assets/pijl-r.svg";
+import dirLeft from "./../assets/pijl-l.svg";
+
 
 const Route = ({ currentDirection }) => {
 
+    const direction = {
+        rechts: dirRight, 
+        links: dirLeft,
+    };
+     // eslint-disable-next-line no-unused-vars
+     const [users, setUsers] = useContext(usersContext);
+     const colors = users.colors;
+
     return (
-        <div className={styles.container}>
-            <p>Route</p>
-            <p>{currentDirection}</p>
+        <div className={styles.container} style={{ borderColor: colors.dark}}>
+            <div className={styles.inside} style={{ borderColor: colors.reg}}>
+                { currentDirection !== "" ? 
+                <>
+                <p className={styles.text} style={{ color: colors.reg }}>{currentDirection}</p> <img className={styles.svg} alt="arrow-direction" src={direction[currentDirection]}/> </>
+                : <p className={styles.text} style={{ color: colors.reg}}>Route</p> }
+            </div>
         </div>
     )
 }; 
